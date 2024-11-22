98ee1e2cefcadaad39de0b0df348e2f567181225
sattwik <maitisattwik@gmail.com>
2024-11-22 20:27:18 +0530

    pg

--- db/db.js
+++ db/db.js
@@ -2,12 +2,17 @@ const { Pool } = require('pg');
 
 // Configure your PostgreSQL database connection
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'voltana',
-  password: 'Sattwik@2002',
+  user: 'sattwik',
+  host: 'dpg-ct09dlq3esus7388egsg-a.singapore-postgres.render.com',
+  database: 'users_ualo',
+  password: 'LQvQoOj4l7h5Kd26G406NdEV8Z5H1X0I',
   port: 5432, // Default PostgreSQL port
+  ssl: {
+    rejectUnauthorized: false, // Allow self-signed certificates
+  },
 });
 
 // Export the pool for use in other files
 module.exports = pool;
+
+//postgresql://sattwik:LQvQoOj4l7h5Kd26G406NdEV8Z5H1X0I@dpg-ct09dlq3esus7388egsg-a.singapore-postgres.render.com/users_ualo
