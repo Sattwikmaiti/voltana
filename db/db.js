86493905f5683f631261192eeff992df327b4676
sattwik <maitisattwik@gmail.com>
2024-11-22 20:59:04 +0530

    pg

--- db/db.js
+++ db/db.js
@@ -16,3 +16,14 @@ const pool = new Pool({
 module.exports = pool;
 
 //postgresql://sattwik:LQvQoOj4l7h5Kd26G406NdEV8Z5H1X0I@dpg-ct09dlq3esus7388egsg-a.singapore-postgres.render.com/users_ualo
+
+/*
+{
+  "name": "Janu",
+  "email": "jenin.doe@example.com",
+  "password": "securepassword",
+  "role": "admin"
+}
+
+
+*/
